b83d666dd93b46029a974e3d7a30cac618574f5f
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-25 18:14:19 -0700

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+
 
 import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
+
+const store = createStore
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
     </div>
   );
 }
+
+// Step 1: create the Redux store
+
+// Step 2: Provide the store
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
