c24560124dc3a775c2dfaf107252420a57e1ef4c
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-25 18:54:07 -0700

    lecture work-in-progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -56,12 +56,16 @@ const mapStateToProps = (state) => {
 }
 
 // build dispatching functions that handle state updates as relevant to this component
-const mapDispatchToProps = (dispatch) => {
-  return {
-    toggleEditing: () => dispatch(toggleEditing()),
-    updateTitle: (newTitle) => dispatch(updateTitle(newTitle))
-  }
-}
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     toggleEditing: () => dispatch(toggleEditing()),
+//     updateTitle: (newTitle) => dispatch(updateTitle(newTitle))
+//   }
+// }
+
+// Shorthand form: we can also say
+const mapDispatchToProps = {toggleEditing, updateTitle}
+// and the bindActionCreators function (part of react-redux) will automatically convert it into the above longform version
 
 export default connect(mapStateToProps, mapDispatchToProps)(Title);
 // connect(mapStateToProps, mapDispatchToProps) returns a HOC (aka, a enhancer function)
