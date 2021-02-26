be20817dbf257c5d1d10a8323ef088f50c708357
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-25 18:40:17 -0700

    lecture work-in-progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -58,7 +58,7 @@ const mapStateToProps = (state) => {
 
 // build dispatching functions that handle state updates as relevant to this component
 const mapDispatchToProps = () => {
-  toggleEditing: dispatch(toggleEditing()),
+  toggleEditing: () => dispatch(toggleEditing()),
   updateTitle: (newTitle) => dispatch(updateTitle(newTitle))
 }
 
